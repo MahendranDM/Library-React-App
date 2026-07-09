0f81933a9181560fa2ca9d5ade672d874c8757af
MahendranDM <mahendrandm2003@gmail.com>
2026-07-09 18:40:26 +0530

    Added Book Adding Feature

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+const AddBook = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12">
+
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4">
+                                <label htmlFor="" className="form-label">  Book Code/ISBN </label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+                                <label htmlFor="" className="form-label"> Title</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+                                <label htmlFor="" className="form-label">Author</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+                                <label htmlFor="" className="form-label">Publisher</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+
+                                <label htmlFor="" className="form-label">Category/Genre</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+
+                                <label htmlFor="" className="form-label">Edition</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+
+                                <label htmlFor="" className="form-label"> Number of Copies </label>
+                                <input type="text" className="form-control" />
+
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+                                <label htmlFor="" className="form-label">Price</label>
+                                <input type="textarea" className="form-control" />
+
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+
+
+                                <label htmlFor="" className="form-label">Publication Year</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4">
+
+
+                                <label htmlFor="" className="form-label"> Shelf Location</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 ">
+
+                                <button className="btn btn-success">Add</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default AddBook
