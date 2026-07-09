d7d86656fff702a7085eee8e64424ea08a6446d1
MahendranDM <mahendrandm2003@gmail.com>
2026-07-09 18:58:08 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import AddBook from './components/AddBook'
+import DeleteBook from './components/DeleteBook'
+import SearchBook from './components/SearchBook'
+import ViewBook from './components/ViewBook'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
+    <BrowserRouter>
+     
 
-      
-
-
-    </>
+      <Routes>
+        <Route path="/" element={<ViewBook />} />
+        <Route path="/add" element={<AddBook />} />
+        <Route path="/search" element={<SearchBook />} />
+        <Route path="/delete" element={<DeleteBook />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
